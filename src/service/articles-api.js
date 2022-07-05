519018db7694959ff6b673f627b8f8f3e316f562
Mykolahist <frichmykola@gmail.com>
2022-07-05 18:52:57 +0300

    makes secured http

--- src/service/articles-api.js
+++ src/service/articles-api.js
@@ -1,3 +1,3 @@
-const BASE_URL = "http://api.spaceflightnewsapi.net/v3/";
+const BASE_URL = "https://api.spaceflightnewsapi.net/v3/";
 
 export const ALL_ARTICLES = BASE_URL + "articles?_limit=500";
